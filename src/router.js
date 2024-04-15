60081209cbd8243c7ee7930c4bf60083b6d4cec7
Vedo-Kodecta <vedad.jahjaefendic@kodecta.com>
2024-04-15 10:55:02 +0200

    Route protection

--- src/router.js
+++ src/router.js
@@ -1,15 +1,11 @@
-// router.js
-
 import { createRouter, createWebHistory } from 'vue-router';
 import AuthForm from './components/AuthForm.vue';
 import DashboardPage from './views/DashboardPage.vue';
 
 const routes = [
-  { path: '/', component: AuthForm, name: 'AuthForm'},
-  { path: '/dashboard', component: DashboardPage, name: 'DashboardPage'}
+  { path: '/', component: AuthForm, name: 'AuthForm' },
+  { path: '/dashboard', component: DashboardPage, name: 'DashboardPage', meta: { requiresAuth: true } }
 ];
-
-
 
 const router = createRouter({
   history: createWebHistory(),
@@ -19,7 +15,14 @@ const router = createRouter({
 router.beforeEach((to, from, next) => {
   console.log('Navigating from:', from.path);
   console.log('Navigating to:', to.path);
-  next(); // Ensure to call next() to continue navigation
+
+  const isAuthenticated = localStorage.getItem('accessToken');
+  
+  if (to.matched.some(record => record.meta.requiresAuth) && !isAuthenticated) {
+    next({ name: 'AuthForm' }); 
+  } else {
+    next(); 
+  }
 });
 
 export default router;
